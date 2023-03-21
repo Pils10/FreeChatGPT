53db38fb0d13a3c4440482dd8112b33b9a9dbb8b
Pils10 <support@pils10.de>
2023-03-21 13:23:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/REPO/',
+  base: '/FreeChatGPT/',
   /* Rest of the configuration here */
   plugins: [react()],
   resolve: {
